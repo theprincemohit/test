49af4ae2425286cad8bc95e96525cae96e9d05ce
MOHIT KUMAR <theprincemohit@gmail.com>
2020-11-17 15:57:44 +0530

    first commit

---
+++ precache-manifest.e2897d124adc06f86682e6af140e66f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8962cd9b5eac9ceb692e3ba7f1de7d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b8521e363624973b095a",
+    "url": "/test/static/css/main.e5857c85.chunk.css"
+  },
+  {
+    "revision": "6cd9d6e8ea7bc7fe7a72",
+    "url": "/test/static/js/2.832fe697.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.832fe697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8521e363624973b095a",
+    "url": "/test/static/js/main.88714469.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
